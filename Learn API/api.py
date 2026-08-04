28d02b0166bbb0ae3fb5f4d919577d02a547fbb5
Justin <j_yang2021@yahoo.com>
2026-08-04 01:07:07 -0700

    Attempted using PokeAPI to pull data and learn how it works

--- Learn API/api.py
+++ Learn API/api.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "https://jsonplaceholder.typicode.com/posts"
-params = {"id": 2}
+url = "https://pokeapi.co/api/v2/pokemon"
+params = {"offset": 0, "limit": 200}
 
 response = requests.get(url, params=params)
 print(response.url)
@@ -12,4 +12,15 @@ print("Status code", response.status_code)
 print("Content-Type:", response.headers.get("Content-Type"))
 
 data = response.json()
+print(data["count"])
+
 print(data)
+for x in data["results"]:
+    print(x)
+    test = requests.get(x["url"])
+    test.raise_for_status()
+    data2 = test.json()
+    print(data2["stats"][0]["base_stat"])
+    print()
+    print()
+    
